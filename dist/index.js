ee7626b5902e7b241f2aba19cbdd563646c80e8e
Johan <me@johanmontorfano.com>
2022-07-18 02:06:52 +0200

    build script fix

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testPack = exports.unit = void 0;
 /** a test-unit is made from this function */
-export function unit(name, runner, expect) {
+function unit(name, runner, expect) {
     console.time(name);
     const result = runner();
     console.timeLog(name, "-> result: ", result === expect ? "👌 Passed" : "❌ Fail");
 }
+exports.unit = unit;
 /** multiple test unit runner, tests to run can be filtered from the `testFilter` prop */
-export function testPack(units, testFilter) {
+function testPack(units, testFilter) {
     units.forEach((_unit) => (testFilter !== undefined && testFilter.indexOf(_unit.name) !== -1) ||
         testFilter === undefined
         ? unit(_unit.name, _unit.runner, _unit.expect)
         : null);
 }
+exports.testPack = testPack;
